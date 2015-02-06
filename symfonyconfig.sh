c5b72f5acdfbea65dd54e7abc0fbcfac62534d3f
Mailam <ijobling@gmail.com>
2015-02-06 09:49:03 +0000

    update bash script

--- symfonyconfig.sh
+++ symfonyconfig.sh
@@ -1,4 +1,7 @@
 echo -e '\E[1;33;44m' "Setting your local timezone - Done"; tput sgr0
 echo date.timezone = UTC >> ~/.parts/etc/php5/php.ini
 
+parts stop apache2
+parts start apache2
+
 rm symfonyconfig.sh
